35953153c1f5508fe312099161ea2faa92aff7a5
auntduda <auntduda@gmail.com>
2021-10-23 20:57:39 -0300

    adicionando as imagens e os arquivos fontes utilizados na construcao do website

---
+++ project/src/js/start-game.js
@@ -0,0 +1,15 @@
+var cartas = [[1,f1],[2,f2],[3,f3],[4,f4],[5,f5],[6,f2],[7,f4],[8,f1],[9,f5],[10,f3]];
+
+console.log("Bem-vindo ao jogo da memoria!")
+
+for(var i=0; i<10; i++){
+    if(i==6){
+        console.log("\n");
+    }
+
+    console.log(i+1);
+}
+
+while(1){
+    console.log("Escolha uma carta: ");
+}
